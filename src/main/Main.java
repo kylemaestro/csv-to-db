6ba48aa4773477dc79c5168adb8876fad3d0c60b
Kyle <48298913+kda150230@users.noreply.github.com>
2020-02-25 16:27:12 -0600

    CLI arg support
    Can specify desired input file in "data" folder through CLI

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,8 +6,19 @@ public class Main {
 		Reader.init("data.csv");
 		
 		DatabaseParser parser = new DatabaseParser();
-		parser.setURL("records");
+		
+		if(args.length != 1) {
+			System.out.println("[ERROR] Usage: 'java -jar csv2db.jar [CSV FILE NAME]'");
+			System.out.println(" -> NOTE: Do not include the file extension. Example for");
+			System.out.println(" -> default CSV file 'records.csv':");
+			System.out.println(" -> java -jar csv2db.jar records");
+			System.exit(1);
+		}
+		
+		parser.setURL(args[0]);
 		parser.createNewDatabaseWithTable();
 		parser.insertAll();
+		
+		System.exit(0);
 	}
 }
